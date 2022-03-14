a15123e52d46d322708b63eece21dae05769eeb5
Adrian Lactuan <aplactuan@gmail.com>
2022-03-14 19:24:02 +0800

    The product variation dropdown

---
+++ app/Http/Livewire/ProductDropdown.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Variation;
+use Livewire\Component;
+
+class ProductDropdown extends Component
+{
+    public $variations;
+
+    public $selectedVariation;
+
+    public function getSelectedVariationModelProperty()
+    {
+        if (!$this->selectedVariation) {
+            return;
+        }
+
+        return Variation::find($this->selectedVariation);
+    }
+
+    public function updatedSelectedVariation()
+    {
+        dd($this->selectedVariationModel);
+    }
+
+    public function render()
+    {
+        return view('livewire.product-dropdown');
+    }
+}
