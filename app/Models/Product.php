c79477cb6985811938b821369b9a168e8105445b
Adrian Lactuan <aplactuan@gmail.com>
2022-03-26 00:42:56 +0800

    Add media library to product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,10 +5,15 @@ namespace App\Models;
 use Cknow\Money\Money;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Product extends Model
+class Product extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
 
     public function formattedPrice()
     {
@@ -19,4 +24,10 @@ class Product extends Model
     {
         return $this->hasMany(Variation::class);
     }
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('thumb200x200')
+            ->fit(Manipulations::FIT_CROP, 200, 200);
+    }''
 }
